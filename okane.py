8fb1a2121c1f54dd6d7b07e55a39325886f279a9
Tomas Karabela <tkarabela@seznam.cz>
2026-05-16 17:09:49 +0200

    chore: Update license year

--- okane.py
+++ okane.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2023 Tomas Karabela
+# Copyright (c) 2023-2026 Tomas Karabela
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
